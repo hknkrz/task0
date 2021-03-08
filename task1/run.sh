e9d52fd54f6a4d604712ea2c89264104a748203c
hknkrz <khadanovich.ba@phystech.edu>
2021-03-08 15:53:41 +0300

    5 fix

--- task1/run.sh
+++ task1/run.sh
@@ -5,10 +5,10 @@ ext=$2
 backup_dir=$3
 archive=$4
 mkdir $backup_dir
-cp -r $dir $backup_dir
-arr=($(find  $backup_dir -type f -name "*.$ext"))
+arr=($(find  $dir -type f -name "*.$ext"))
 for f in ${arr[@]}
 do
+cp $f $backup_dir
 var1=$(basename $f)
 for j in ${arr[@]}
 do
@@ -22,20 +22,12 @@ filename=$(basename $f)
 len_filename=${#filename}
 full_adress=${f::-$len_filename}
 slash_to_backslash=${f//$slash/\\}
-cn=$full_adress$slash_to_backslash
-mv $f $cn
+mv $backup_dir$filename $backup_dir$slash_to_backslash
 fi
 fi
 done
 done
-for x in $(find $backup_dir -type f -not -name "*.$ext")
-	do
-		rm $x
-	done
-for z in $(find $backup_dir -type d -empty)
-	do
-		rmdir $z
-	done
+
 tar -czpf $archive $backup_dir
 echo done
 
