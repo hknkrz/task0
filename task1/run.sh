67d2f922f4e2fc913938430e5842c8502754b3bb
hknkrz <khadanovich.ba@phystech.edu>
2021-03-09 00:51:54 +0300

    n+1 commit

--- task1/run.sh
+++ task1/run.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-slash="/"
 dir=$1
 ext=$2
 backup_dir=$3
 archive=$4
 mkdir $backup_dir
-for f in $(find  $dir -type f -name "*.$ext")
+arr=($(find $dir -type f -name "*.$ext"))
+for x in ${arr[@]}
 do
-if [ -f $f ]
-then
-var1=$(basename $f)
-filename=$(basename $f)
-len_filename=${#filename}
-full_adress=${f::-$len_filename}
-slash_to_backslash=${f//$slash/\\}
-cp $f $backup_dir$slash_to_backslash
-else
-cp $f $backup_dir
-fi
+cp --backup=t $x $backup_dir
 done
-
 tar -czpf $archive $backup_dir
 echo done
