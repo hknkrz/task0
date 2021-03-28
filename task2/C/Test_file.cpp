d5b12a3c403b69c0f2fcd18653506aca391121bd
hknkrz <khadanovich.ba@phystech.edu>
2021-03-28 21:57:47 +0300

    GTest integration 2
    GTest integration 3
    
    Gtest integration 4
    
    Gtest integration 5
    
    Gtest integration 6
    
    Gtest integration 7
    
    Gtest integration 8
    
    Gtest integration 9
    
    Gtest integration 10
    
    Gtest integration 11
    
    Gtest integration 12
    
    Gtest integration 13
    
    Gtest integration 14
    
    Gtest integration 15
    
    Gtest integration 16
    
    Gtest integration 17
    
    Gtest integration 18
    
    Gtest integration 19
    
    Gtest integration 20
    
    Gtest integration 21
    
    Gtest integration 22
    
    Gtest integration 23
    
    Gtest integration 24
    
    Gtest integration 25
    
    Gtest integration 26
    
    Gtest integration 27
    
    Gtest integration 28
    
    Gtest integration 29
    
    Gtest integration 30
    
    Gtest integration 31
    
    Gtest integration 32
    
    Gtest integration 33
    
    Gtest integration 34
    
    Gtest integration 35
    
    Gtest integration 36
    
    Gtest integration 37
    
    Gtest integration 38
    
    Gtest integration 39
    
    Gtest integration 40
    
    Gtest integration 41
    
    Gtest integration 42
    
    Gtest integration 43
    
    Gtest integration 44
    
    Gtest integration 45
    
    Gtest integration 46
    
    Gtest integration 47
    
    Gtest integration 48

--- task2/C/Test_file.cpp
+++ task2/C/Test_file.cpp
@@ -1,30 +1,30 @@
 #include <gtest/gtest.h>
-#include "B/lib.h"
-#include "A/index.h"
-TEST(And){
-	EXPECT_EQ(0,And(0,0)
+#include "main.h"
+class Testing_class : public ::testing::Test {};
+TEST(Testing_class,And1){
+	EXPECT_EQ(0,And(0,0));
 }
-TEST(And){
-	EXPECT_EQ(0,And(1,0)
+TEST(Testing_class,And2){
+	EXPECT_EQ(0,And(1,0));
 }
-TEST(And){
-	EXPECT_EQ(0, And(0,1)
+TEST(Testing_class,And3){
+	EXPECT_EQ(0, And(0,1));
 }
-TEST(And){
-	EXPECT_EQ(1, And(1,1)
+TEST(Testing_class,And4){
+	EXPECT_EQ(1, And(1,1));
 }
-TEST(And){
-	EXPECT_EQ(1, And(12,13)
+TEST(Testing_class,And5){
+	EXPECT_EQ(1, And(12,13));
 }
-TEST(production){
-	EXPECT_EQ(0, production(0,0)
+TEST(Testing_class,production1){
+	EXPECT_EQ(0, production(0,0));
 }
-TEST(production){
-	EXPECT_EQ(0, production(0,23)
+TEST(Testing_class,production2){
+	EXPECT_EQ(0, production(0,23));
 }
-TEST(production){
-	EXPECT_EQ(0, production(23,0)
+TEST(Testing_class,production3){
+	EXPECT_EQ(0, production(23,0));
 }
-TEST(production){
-	EXPECT_EQ(4, production(2,2)
+TEST(Testing_class,production4){
+	EXPECT_EQ(4, production(2,2));
 }
