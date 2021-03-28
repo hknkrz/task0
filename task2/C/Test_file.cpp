d98a56ad614da10098f0723a88711ca4821a7efb
hknkrz <khadanovich.ba@phystech.edu>
2021-03-28 21:53:52 +0300

    GTest integration 1

---
+++ task2/C/Test_file.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include "B/lib.h"
+#include "A/index.h"
+TEST(And){
+	EXPECT_EQ(0,And(0,0)
+}
+TEST(And){
+	EXPECT_EQ(0,And(1,0)
+}
+TEST(And){
+	EXPECT_EQ(0, And(0,1)
+}
+TEST(And){
+	EXPECT_EQ(1, And(1,1)
+}
+TEST(And){
+	EXPECT_EQ(1, And(12,13)
+}
+TEST(production){
+	EXPECT_EQ(0, production(0,0)
+}
+TEST(production){
+	EXPECT_EQ(0, production(0,23)
+}
+TEST(production){
+	EXPECT_EQ(0, production(23,0)
+}
+TEST(production){
+	EXPECT_EQ(4, production(2,2)
+}
